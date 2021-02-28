4b707869ff81357cc574cfd5a7ead3756a73b38c
Emile-Nsengimana <emilereas7@gmail.com>
2021-02-28 11:14:35 +0200

    removed console

--- src/controllers/HotelResto.js
+++ src/controllers/HotelResto.js
@@ -45,7 +45,6 @@ class HotelRestoManager {
         items: hotelRestos,
       });
     } catch (error) {
-      console.log(error);
       return res.status(500).send({
         error: 'Server error',
       });
